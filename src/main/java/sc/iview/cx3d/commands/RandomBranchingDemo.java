371c72c424394ebb84cee8533cba4391ef5615e4
tferr <tiago.tferr@gmail.com>
2020-06-13 00:28:24 -0400

    Allow RandomBranchingDemo to run from menu
    While at it:
    - cleanup code a bit
    - merge 0c503a40545941
    - Pom: Update snt version

--- src/main/java/sc/iview/cx3d/commands/RandomBranchingDemo.java
+++ src/main/java/sc/iview/cx3d/commands/RandomBranchingDemo.java
@@ -28,11 +28,24 @@
  */
 package sc.iview.cx3d.commands;
 
+import static sc.iview.commands.MenuWeights.DEMO;
+import static sc.iview.commands.MenuWeights.DEMO_LINES;
+import static sc.iview.cx3d.utilities.Matrix.randomNoise;
+
+import org.scijava.Context;
+import org.scijava.command.Command;
+import org.scijava.command.CommandService;
+import org.scijava.plugin.Menu;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.util.Colors;
+
 import graphics.scenery.SceneryBase;
-import org.jgrapht.graph.DefaultDirectedGraph;
-import org.scijava.log.LogLevel;
-import org.scijava.log.LogService;
-import org.scijava.ui.UIService;
+import net.imagej.ImageJ;
+import sc.fiji.snt.SNTService;
+import sc.fiji.snt.SciViewSNT;
+import sc.fiji.snt.Tree;
+import sc.iview.SciView;
 import sc.iview.cx3d.Param;
 import sc.iview.cx3d.cells.Cell;
 import sc.iview.cx3d.cells.CellFactory;
@@ -40,34 +53,7 @@ import sc.iview.cx3d.localBiology.NeuriteElement;
 import sc.iview.cx3d.simulations.ECM;
 import sc.iview.cx3d.simulations.Scheduler;
 import sc.iview.cx3d.simulations.tutorial.RandomBranchingModule;
-import io.scif.SCIFIOService;
-import net.imagej.ImageJService;
-import org.jgrapht.graph.DefaultDirectedWeightedGraph;
-import org.jgrapht.graph.DefaultWeightedEdge;
-import org.scijava.Context;
-import org.scijava.ItemIO;
-import org.scijava.command.Command;
-import org.scijava.plugin.Menu;
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
-import org.scijava.service.SciJavaService;
-import org.scijava.thread.ThreadService;
-import org.scijava.util.Colors;
-import sc.fiji.snt.SNTService;
-import sc.fiji.snt.Tree;
-import sc.fiji.snt.analysis.graph.GraphUtils;
-import sc.fiji.snt.util.SWCPoint;
-import sc.fiji.snt.viewer.Viewer3D;
-import sc.iview.SciView;
-import sc.iview.SciViewService;
 import sc.iview.cx3d.utilities.ConvertUtils;
-
-import java.util.HashMap;
-import java.util.Vector;
-
-import static sc.iview.cx3d.utilities.Matrix.randomNoise;
-import static sc.iview.commands.MenuWeights.DEMO;
-import static sc.iview.commands.MenuWeights.DEMO_LINES;
 
 /**
  * Random branching demo from Cx3D
@@ -75,103 +61,86 @@ import static sc.iview.commands.MenuWeights.DEMO_LINES;
  * @author Kyle Harrington
  */
 @Plugin(type = Command.class, label = "Random Branching", menuRoot = "SciView", //
-        menu = { @Menu(label = "Demo", weight = DEMO), //
-                 @Menu(label = "Cx3D", weight = DEMO), //
-                 @Menu(label = "Random Branching", weight = DEMO_LINES) })
+		menu = { @Menu(label = "Demo", weight = DEMO), //
+				@Menu(label = "Cx3D", weight = DEMO), //
+				@Menu(label = "Random Branching", weight = DEMO_LINES) })
 public class RandomBranchingDemo implements Command {
 
-    @Parameter
-    private SciView sciView;
+	@Parameter
+	private SciView sciView;
 
-    @Parameter
-    private Context context;
+	@Parameter
+	private Context context;
 
-    @Parameter
-    private SNTService sntService;
+	@Parameter
+	private SNTService sntService;
 
-    //@Parameter(type = ItemIO.OUTPUT)
+	@Parameter(label = "Simulation end time")
+	private float maxTime = 2;
 
-    @Parameter
-    private Tree tree;
-
-    @Parameter(label = "Simulation end time")
-    private float maxTime = 2;
-
-    @Parameter(label = "Growth speed")
-    private float speed = 100;
+	@Parameter(label = "Growth speed")
+	private float speed = 100;
 
 	@Parameter(label = "Probability to bifurcate")
 	private double probabilityToBifurcate = 0.005; // o.oo5
 
-    @Parameter(label = "Probability to branch")
+	@Parameter(label = "Probability to branch")
 	private double probabilityToBranch = 0.005;
 
-    @Override
-    public void run() {
-        //ECM ecm = ECM.getInstance(getContext());
-        ECM ecm = ECM.getInstance(context);
+	@Override
+	public void run() {
+		// ECM ecm = ECM.getInstance(getContext());
+		ECM ecm = ECM.getInstance(context);
 
-        ECM.setRandomSeed(7L);
+		ECM.setRandomSeed(7L);
 
-        ecm.clearAll();
-        ecm.resetTime();
-        ecm.getSciViewCX3D().clear();
+		ecm.clearAll();
+		ecm.resetTime();
+		ecm.getSciViewCX3D().clear();
 		for (int i = 0; i < 18; i++) {
-			ecm.getPhysicalNodeInstance(randomNoise(1000,3));
+			ecm.getPhysicalNodeInstance(randomNoise(1000, 3));
 		}
 
-        Cell c = CellFactory.getCellInstance(randomNoise(40, 3));
-        c.setColorForAllPhysicalObjects(Param.GRAY);
-        double[] pos = c.getSomaElement().getLocation();
-        NeuriteElement neurite = c.getSomaElement().extendNewNeurite(new double[] {0,0,1});
-        neurite.getPhysicalCylinder().setDiameter(2);
+		Cell c = CellFactory.getCellInstance(randomNoise(40, 3));
+		c.setColorForAllPhysicalObjects(Param.GREEN);
+		NeuriteElement neurite = c.getSomaElement().extendNewNeurite(new double[] { 0, 0, 1 });
+		neurite.getPhysicalCylinder().setDiameter(2);
 
-        RandomBranchingModule branchingModule = new RandomBranchingModule();
-        branchingModule.setSpeed(speed);
-        branchingModule.setProbabilityToBranch(probabilityToBranch);
-        branchingModule.setProbabilityToBifurcate(probabilityToBifurcate);
-        neurite.addLocalBiologyModule(branchingModule);
+		RandomBranchingModule branchingModule = new RandomBranchingModule();
+		branchingModule.setSpeed(speed);
+		branchingModule.setProbabilityToBranch(probabilityToBranch);
+		branchingModule.setProbabilityToBifurcate(probabilityToBifurcate);
+		neurite.addLocalBiologyModule(branchingModule);
 
 		Scheduler.simulate(maxTime);
 
 		sciView.centerOnNode(ecm.getSciViewCX3D().getCx3dGroup());
 
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
-        // This should work for Cx3D trees
-        Tree realtree = ConvertUtils.cellToTree(c);
-        realtree.setLabel("Cx3D_Tree");
-        realtree.setColor(Colors.RED);
+		Tree realtree = ConvertUtils.cellToTree(c);
+		realtree.setLabel("Cx3D_Tree");
+		realtree.setColor(Colors.RED);
 
-        tree.merge(realtree);
-//        sntService.initialize(true);
-//        sntService.getPathAndFillManager().clear();
-//        sntService.loadTree(tree);
+		SciViewSNT sciviewSNT = sntService.getOrCreateSciViewSNT();
+		if (sciView != null) {
+			sciviewSNT.setSciView(sciView);
+			sciView.waitForSceneInitialisation();
+		}
+		//realtree.setLabel("Cx3D_Tree (SNT)");
+		sciviewSNT.addTree(realtree);
+	}
 
-    }
-
-    public static void main( String... args ) {
-        SceneryBase.xinitThreads();
-
-        System.setProperty( "scijava.log.level:sc.iview", "debug" );
-        Context context = new Context( ImageJService.class, SciJavaService.class, SCIFIOService.class, ThreadService.class);
-
-        UIService ui = context.service( UIService.class );
-        if( !ui.isVisible() ) ui.showUI();
-
-        // Currently Cx3D demos need to make their own SciView instance
-        SciViewService sciViewService = context.service( SciViewService.class );
-        try {
-            SciView sciView = sciViewService.getOrCreateActiveSciView();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-//        CommandService commandService = context.service(CommandService.class);
-//        commandService.run(RandomBranchingDemo.class,true,new Object[]{});
-    }
+	public static void main(String... args) {
+		SceneryBase.xinitThreads();
+		System.setProperty( "scijava.log.level:sc.iview", "debug" );
+		final ImageJ ij = new ImageJ();
+		ij.ui().showUI();
+		final CommandService cmdService = ij.context().getService(CommandService.class);
+		cmdService.run(RandomBranchingDemo.class, true);
+	}
 }
